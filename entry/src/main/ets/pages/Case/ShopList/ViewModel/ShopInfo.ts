3eeec650265bc0d1368bdfcbb145c9c3dc028ed5
wangfd <wangfd@gyist.com>
2024-04-25 12:12:03 +0800

    "MF"

---
+++ entry/src/main/ets/pages/Case/ShopList/ViewModel/ShopInfo.ts
@@ -0,0 +1,12 @@
+
+export default class ShopInfo {
+  id: number
+  name: string
+  images: string[]
+  area: string
+  address: string
+  avgPrice: number
+  comments: number
+  score: number
+  openHours: string
+}
---
+++ entry/src/main/ets/pages/Case/ShopList/ViewModel/ShopModel.ts
@@ -0,0 +1,36 @@
+import axios from '@ohos/axios'
+import ShopInfo from './ShopInfo';
+
+class ShopModel{
+  baseURL: string = 'http://192.168.1.177:9588'
+  pageNo: number = 1
+
+  getShopList(): Promise<ShopInfo[]>{
+    return new Promise((resolve, reject) => {
+      axios.get(
+        `${this.baseURL}/shops`,
+        {
+          params: {pageNo: this.pageNo, pageSize: 4}
+        }
+      )
+        .then(resp => {
+          if(resp.status === 200){
+            // 查询成功
+            console.log('查询商铺成功！', JSON.stringify(resp.data))
+            resolve(resp.data)
+          }else{
+            console.log('查询商铺信息失败！error:', JSON.stringify(resp))
+            reject('查询商铺失败')
+          }
+        })
+        .catch(error => {
+          console.log('查询商铺信息失败！error:', JSON.stringify(error))
+          reject('查询商铺失败')
+        })
+    })
+  }
+
+}
+
+const shopModel = new ShopModel();
+export default shopModel as ShopModel;
